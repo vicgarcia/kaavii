04b24402bd4816fbbe1cbaa7253df924127d372f
Vic Garcia <vic.garcia@outlook.com>
2015-10-06 07:42:14 +0000

    add testing and documentation for storage

--- source/Storage.php
+++ source/Storage.php
@@ -1,12 +1,23 @@
 <?php
+
 namespace KaaVii;
 
+/**
+ * the Kaavii\Storage class implements an incredibly simple \Redis
+ * backed key-value storage system for serialized PHP values/objects
+ *
+ **/
 class Storage
 {
     protected
         $prefix,
         $redis;
 
+    /**
+     * provide a configured \Redis object and a prefix string to use
+     * for redis keys for the stored values
+     *
+     **/
     public function __construct(\Redis $redis, $prefix = '')
     {
         $this->redis = $redis;
@@ -14,6 +25,11 @@ class Storage
                 ? $prefix : $prefix . ':';
     }
 
+    /**
+     *  get the value stored at a key
+     *
+     *  returns the value
+     **/
     public function get($key)
     {
         $serialized = $this->redis->get($this->formatKey($key));
@@ -22,23 +38,61 @@ class Storage
                 ? false : unserialize($serialized);
     }
 
+    /**
+     * set the value stored at a key to value, serializing it
+     *
+     * returns true on success, false on failure
+     **/
     public function set($key, $value)
     {
         return $this->redis->set($this->formatKey($key), serialize($value));
     }
 
+    /**
+     * delete value stored at a key
+     *
+     * returns true on success, false on failure
+     **/
     public function delete($key)
     {
         return $this->redis->delete($this->formatKey($key));
     }
 
+    /**
+     * retrieve a list of all keys stored, with respect to prefix
+     *
+     * this will throw an exception if the prefix is empty
+     * in order to prevent over-exposing keys
+     *
+     * returns an array
+     **/
     public function keys()
     {
+        if (empty($this->prefix)) {
+           throw new StorageException('empty prefix w/ clear() delete everything');
+        }
+
         return $this->redis->keys($this->prefix . '*');
     }
 
+    /**
+     * formats a string by appending it to the prefix with seperating colon
+     * using the prefix allow us to namespace the data stored by Storage in Redis
+     *
+     * returns a constructed string in the form of 'prefix:key'
+     **/
     public function formatKey($key)
     {
         return $this->prefix . $key;
     }
+}
+
+
+/**
+ * the Kaavii\StorageException typed exception, used for Kaavii\Storage related errors
+ *
+ **/
+class StorageException extends \Exception
+{
+
 }
--- test/StorageTest.php
+++ test/StorageTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace KaaVii;
 
 class StorageTest extends \PHPUnit_Framework_TestCase
@@ -6,34 +7,123 @@ class StorageTest extends \PHPUnit_Framework_TestCase
 
     public function testPrefixWithoutTrailingColon()
     {
-        $redis = $this->getMock('\Redis');
+        $storage = new Storage($this->getMock('\Redis'), 'prefix');
 
-        $storage = new Storage($redis, 'prefix');
         $this->assertEquals('prefix:key', $storage->formatKey('key'));
     }
 
     public function testPrefixWithTrailingColon()
     {
-        $redis = $this->getMock('\Redis');
+        $storage = new Storage($this->getMock('\Redis'), 'prefix:');
 
-        $storage = new Storage($redis, 'prefix:');
         $this->assertEquals('prefix:key', $storage->formatKey('key'));
     }
 
     public function testFormatKeyWithoutPrefix()
     {
-        $redis = $this->getMock('\Redis');
+        $storage = new Storage($this->getMock('\Redis'), '');
 
-        $storage = new Storage($redis, '');
         $this->assertEquals('key', $storage->formatKey('key'));
     }
 
     public function testFormatKeyWithPrefix()
     {
-        $redis = $this->getMock('\Redis');
+        $storage = new Storage($this->getMock('\Redis'), 'prefix');
+
+        $this->assertEquals('prefix:key', $storage->formatKey('key'));
+    }
+
+    public function testGetStringValueAtKey()
+    {
+        $expectedString = 'expected cache content';
+        $serializedString = serialize($expectedString);
+
+        $redis = $this->getMockBuilder('\Redis')->getMock();
+        $redis->method('get')->willReturn($serializedString);
 
         $storage = new Storage($redis, 'prefix');
-        $this->assertEquals('prefix:key', $storage->formatKey('key'));
+        $this->assertEquals($expectedString, $storage->get('unchecked:key'));
+    }
+
+    public function testLoadArrayValueAtKey()
+    {
+        $expectedArray = [ 'an', 'array', 'value' ];
+        $serializedArray = serialize($expectedArray);
+
+        $redis = $this->getMockBuilder('\Redis')->getMock();
+        $redis->method('get')->willReturn($serializedArray);
+
+        $storage = new Storage($redis, 'prefix');
+        $this->assertEquals($expectedArray, $storage->get('unchecked:key'));
+    }
+
+    public function testLoadInvalidValueAtKey()
+    {
+        $redis1 = $this->getMockBuilder('\Redis')->getMock();
+        $redis1->method('get')->willReturn(false);
+
+        $storage = new Storage($redis1, 'prefix');
+        $this->assertEquals(false, $storage->get('unchecked:key'));
+
+        $redis2 = $this->getMockBuilder('\Redis')->getMock();
+        $redis2->method('get')->willReturn([]);
+
+        $storage = new Storage($redis2, 'prefix');
+        $this->assertEquals(false, $storage->get('unchecked:key'));
+    }
+
+    public function testSet()
+    {
+        $value = 'a string value';
+
+        $redis = $this->getMockBuilder('\Redis')->getMock();
+        $redis->expects($this->once())
+            ->method('set')
+            ->with('prefix:key', serialize($value))
+            ->willReturn(true);
+
+        $storage = new Storage($redis, 'prefix');
+
+        $this->assertTrue($storage->set('key', $value));
+    }
+
+    public function testDeleteCallsRedisDeleteMethod()
+    {
+        $redis = $this->getMockBuilder('\Redis')->getMock();
+        $redis->expects($this->once())
+            ->method('delete')
+            ->with('prefix:key')
+            ->willReturn(true);
+
+        $storage = new Storage($redis, 'prefix');
+
+        $this->assertTrue($storage->delete('key'));
+    }
+
+    public function testKeysGetsArrayFromRedis()
+    {
+        $expectedArray = [ 'an', 'array', 'value' ];
+
+        $redis = $this->getMockBuilder('\Redis')->getMock();
+        $redis->expects($this->once())
+            ->method('keys')
+            ->with('prefix:*')
+            ->willReturn($expectedArray);
+
+        $storage = new Storage($redis, 'prefix');
+
+        $this->assertEquals($expectedArray, $storage->keys());
+    }
+
+    public function testKeysErrorWithEmptyPrefix()
+    {
+        $redis = $this->getMockBuilder('\Redis')->getMock();
+
+        // we expect the exception is thrown, fails if it isn't
+        $this->setExpectedException('Kaavii\StorageException');
+
+        $storage = new Storage($redis, '');
+        $keys = $storage->keys();
     }
 
 }
